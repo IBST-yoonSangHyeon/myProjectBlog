0caaec49ba18eba56491408568ba80f365b40be4
IBST-yoonSangHyeon <dalpework@gmail.com>
2021-10-14 22:28:55 +0900

    버튼에 기능개발을 해보자 & 리액트 state변경하는 법

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -6,6 +7,7 @@ function App() {
   let posts = '강남 고기 맛집';
 
   let [글제목, 글제목변경] =  useState(['남자 코트 추천', '강남 우동 맛집', '윤상현 짱짱짱']); // ES6 destructuring 문법
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
@@ -13,12 +15,7 @@ function App() {
         <div style={ { color: 'blue', fontSize : '30px' } }>개발 Blog</div>
       </div>
       <div className="list">
-        <h3>{ posts }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{ 글제목[0] }</h3>
+        <h3>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
