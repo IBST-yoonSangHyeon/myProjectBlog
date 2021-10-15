d4717fe68e14fa1896923ab5dfd5736c6131b137
IBST-yoonSangHyeon <dalpework@gmail.com>
2021-10-15 14:00:30 +0900

    (UI 제작 패턴) props를 응용한 상세페이지 만들기

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
 
   let [글제목, 글제목변경] =  useState(['남자 코트 추천', '강남 우동 맛집', '윤상현 짱짱짱']); // ES6 destructuring 문법
   let [따봉, 따봉변경] = useState([0, 0, 0]);
-  let [modal, modal변경] = useState(false); // 모달창 on/off 스위치 (모달에서 많이 사용함)
+  let [누른제목, 누른제목변경] = useState(0);
+  let [modal, modal변경] = useState(true); // 모달창 on/off 스위치 (모달에서 많이 사용함)
 
   // 자주 사용됨
   // function 반복된UI () {
@@ -46,17 +47,23 @@ function App() {
           console.log('인덱스', 인덱스);
           return (
           <div className="list">
-            <h3>{ 글 } <span onClick={ () => { 따봉증가(인덱스) } }>👍</span> { 따봉[인덱스] } </h3>
+            <h3 onClick={ () => { 누른제목변경(인덱스) } } >{ 글 } <span onClick={ () => { 따봉증가(인덱스) } }>👍</span> { 따봉[인덱스] } </h3>
             <p>2월 17일 발행</p>
             <hr/>
           </div>
           );
         })
       }
+
+      {/* <button onClick={ () => { 누른제목변경(0) } } >버튼1</button>
+      <button onClick={ () => { 누른제목변경(1) } }>버튼2</button>
+      <button onClick={ () => { 누른제목변경(2) } }>버튼3</button> */}
+
       <button onClick={ () => {modal변경(!modal)} }>열고 닫기</button>
+      
       { 
         modal === true 
-        ? <Modal 글제목={글제목}/>
+        ? <Modal 글제목={글제목} 누른제목={누른제목} />
         : null
       }
 
@@ -67,7 +74,7 @@ function App() {
 function Modal(props) {
   return (
     <div className="modal">
-      <h2>{ props.글제목[0] }</h2>
+      <h2>{ props.글제목[props.누른제목] }</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
