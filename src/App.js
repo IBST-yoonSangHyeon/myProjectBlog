b9155b9f7fa48bcadcdf79f5b700c9f8ae67fba4
IBST-yoonSangHyeon <dalpework@gmail.com>
2021-10-15 10:40:41 +0900

    숙제 해설 : 블로그 글 수정버튼 만들기

--- src/App.js
+++ src/App.js
@@ -9,8 +9,16 @@ function App() {
   let [글제목, 글제목변경] =  useState(['남자 코트 추천', '강남 우동 맛집', '윤상현 짱짱짱']); // ES6 destructuring 문법
   let [따봉, 따봉변경] = useState(0);
 
+  function 제목바꾸기() {
+    // array또는 object state변경시 복사본(deep copy) 만든다.
+    let newArray = [...글제목]; // deep copy(객체, 배열 가능) -> ...
+    newArray[0] = '여자코트 추천';
+    글제목변경(newArray);
+  }
+
   return (
     <div className="App">
+      <button onClick={ 제목바꾸기 }>클릭</button>
       <div className="black-nav">
         <div style={ { color: 'blue', fontSize : '30px' } }>개발 Blog</div>
       </div>
