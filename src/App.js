327eaf9c3f6b7c983d23d30842d9ac8769c81bf6
IBST-yoonSangHyeon <dalpework@gmail.com>
2021-10-15 10:54:14 +0900

    React Component : 많은 div들을 한 단어로 줄이고 싶은 충동이 들 때

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let posts = '강남 고기 맛집';
+  
 
   let [글제목, 글제목변경] =  useState(['남자 코트 추천', '강남 우동 맛집', '윤상현 짱짱짱']); // ES6 destructuring 문법
   let [따봉, 따봉변경] = useState(0);
+  let posts = '강남 고기 맛집';
 
-  function 제목바꾸기() {
-    // array또는 object state변경시 복사본(deep copy) 만든다.
-    let newArray = [...글제목]; // deep copy(객체, 배열 가능) -> ...
-    newArray[0] = '여자코트 추천';
-    글제목변경(newArray);
-  }
+  // function 제목바꾸기() {
+  //   // array또는 object state변경시 복사본(deep copy) 만든다.
+  //   let newArray = [...글제목]; // deep copy(객체, 배열 가능) -> ...
+  //   newArray[0] = '여자코트 추천';
+  //   글제목변경(newArray);
+  // }
 
   return (
     <div className="App">
-      <button onClick={ 제목바꾸기 }>클릭</button>
       <div className="black-nav">
         <div style={ { color: 'blue', fontSize : '30px' } }>개발 Blog</div>
       </div>
@@ -37,8 +37,19 @@ function App() {
         <p>2월 17일 발행</p>
         <hr/>
       </div>
+      <Modal />
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2> { 글제목[0] }</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
