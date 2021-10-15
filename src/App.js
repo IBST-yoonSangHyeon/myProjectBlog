19a56d3a0de5a1caed069d67252df3125c357443
IBST-yoonSangHyeon <dalpework@gmail.com>
2021-10-15 12:07:54 +0900

    map : 많은 div들을 반복문으로 줄이고 싶은 충동이 들 때

--- src/App.js
+++ src/App.js
@@ -7,43 +7,52 @@ function App() {
   
 
   let [글제목, 글제목변경] =  useState(['남자 코트 추천', '강남 우동 맛집', '윤상현 짱짱짱']); // ES6 destructuring 문법
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, modal변경] = useState(false); // 모달창 on/off 스위치 (모달에서 많이 사용함)
+
+  // 자주 사용됨
+  // function 반복된UI () {
+  //   let 어레이 = [];
+  //   for (let i = 0; i < 어레이.length; i++){
+  //     어레이.push(<div>안녕</div>);
+  //   }
+  //   return 어레이;
+  // }
+
+  function 따봉증가(인덱스) {
+    let 뉴따봉 = [...따봉];
+    뉴따봉[인덱스]++;
+    console.log(뉴따봉);
+    따봉변경(뉴따봉);
+  }
+
   let posts = '강남 고기 맛집';
 
-  // function 제목바꾸기() {
+  // 자주 사용됨
+  // function 제목바꾸기() { 
   //   // array또는 object state변경시 복사본(deep copy) 만든다.
   //   let newArray = [...글제목]; // deep copy(객체, 배열 가능) -> ...
   //   newArray[0] = '여자코트 추천';
   //   글제목변경(newArray);
   // }
 
-  function 모달스위치 () {
-    modal변경(!modal);
-  }
-
   return (
     <div className="App">
       <div className="black-nav">
         <div style={ { color: 'blue', fontSize : '30px' } }>개발 Blog</div>
       </div>
-      <div className="list">
-        <h3>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{ 글제목[1] }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3 onClick={ () => { modal변경(!modal); } }>{ 글제목[2] }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-
-      <button onClick={ 모달스위치 }>버튼</button>
+      { 
+        글제목.map((글, 인덱스, 어레이이) => { 
+          console.log('인덱스', 인덱스);
+          return (
+          <div className="list">
+            <h3>{ 글 } <span onClick={ () => { 따봉증가(인덱스) } }>👍</span> { 따봉[인덱스] } </h3>
+            <p>2월 17일 발행</p>
+            <hr/>
+          </div>
+          );
+        })
+      }
 
       { 
         modal === true 
